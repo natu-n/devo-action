03439dff369600272870974209cf3fc44ae258a3
natu-n <natu0819@gmail.com>
2019-09-20 09:00:11 +0900

    Create vue.config.js

---
+++ vue.config.js
@@ -0,0 +1,12 @@
+const path = require('path')
+
+module.exports = {
+  publicPath: './',
+  configureWebpack: {
+    resolve: {
+      alias: {
+        vue$: path.resolve('./node_modules/vue/dist/vue.common.js')
+      }
+    }
+  }
+}
