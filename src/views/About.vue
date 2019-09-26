0a26ec92cde974a41e8ad065b829def8a9c6358e
natu_n <natu0819+git@gmail.com>
2019-09-26 11:12:20 +0900

    last month dot color

--- src/views/About.vue
+++ src/views/About.vue
@@ -2,22 +2,21 @@
   <v-card class="mx-auto" max-width="600" elevation="0">
     <v-row justify="space-around" v-if="$store.state.loading">
       <div>
-        <div class="subheading">Until last month</div>
+        <div class="title">Until last month</div>
         <v-date-picker
           v-model="pastDate"
           :max="lastPastDate"
-          :events="arrayEvents"
+          :events="functionEvents"
           event-color="green lighten-1"
-          color="blue lighten-4"
+          color="blue lighten-3"
           header-color="green lighten-1"
         ></v-date-picker>
       </div>
       <div>
-        <div class="subheading">this month</div>
+        <div class="title">this month</div>
         <v-date-picker
           v-model="today"
           :disabled="true"
-          :event-color="date => (date[9] % 2 ? 'red' : 'yellow')"
           :events="functionEvents"
           color="blue lighten-5"
           header-color="primary"
@@ -46,22 +45,41 @@ export default {
     this.lastPastDate = dayjs(this.pastDate)
       .endOf('month')
       .format('YYYY-MM-DD')
-
-    this.arrayEvents = [...Array(6)].map(() => {
-      const day = Math.floor(Math.random() * 30)
-      const d = new Date()
-      d.setDate(day)
-      return d.toISOString().substr(0, 10)
-    })
   },
 
   methods: {
     functionEvents(date) {
-      console.log(date)
-      const [, , day] = date.split('-')
-      if ([12, 17, 28].includes(parseInt(day, 10))) return true
-      if ([1, 19, 22].includes(parseInt(day, 10))) return ['red', '#00f']
-      return false
+      const dotColor = []
+      const ix = this.$store.state.info.findIndex(function(
+        element,
+        index,
+        array
+      ) {
+        return date == element.date
+      })
+      if (ix > 0) {
+        const diastolic = this.$store.state.info[ix].diastolic
+        const systolic = this.$store.state.info[ix].systolic
+
+        if (diastolic > 84) {
+          dotColor.push('red')
+        } else if (diastolic > 79) {
+          dotColor.push('orange')
+        } else {
+          dotColor.push('green')
+        }
+
+        if (systolic > 134) {
+          dotColor.push('red')
+        } else if (systolic > 129) {
+          dotColor.push('orange')
+        } else {
+          dotColor.push('green')
+        }
+        return dotColor
+      } else {
+        return false
+      }
     }
   }
 }
